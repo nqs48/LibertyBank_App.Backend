08421795d4322a1265ee648ea4b519b4e8abc8aa
Juan Pablo Cano <canojuan0415@gmail.com>
2023-02-28 15:16:26 -0500

    Feat: Added Usuario handler and command

---
+++ TallerBackGrupalBOT/src/Infrastructure/EntryPoints/EntryPoints.ReactiveWeb/Entities/Handlers/UsuarioResponse.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Model.Entities.Usuarios;
+
+namespace EntryPoints.ReactiveWeb.Entities.Handlers;
+
+/// <summary>
+/// Request DTO de entidad <see cref="Usuario"/>
+/// </summary>
+public class UsuarioResponse
+{
+    /// <summary>
+    /// Id
+    /// </summary>
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Nombre completo
+    /// </summary>
+    public string NombreCompleto { get; set; }
+
+    /// <summary>
+    /// Rol
+    /// </summary>
+    public Roles Rol { get; set; }
+}

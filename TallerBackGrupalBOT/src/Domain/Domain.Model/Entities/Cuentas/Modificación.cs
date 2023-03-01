fde1731bbc49813921c373f3cfda933b1bad3916
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-02-28 22:46:08 -0500

    Documantacion de clases y enums

--- TallerBackGrupalBOT/src/Domain/Domain.Model/Entities/Cuentas/Modificación.cs
+++ TallerBackGrupalBOT/src/Domain/Domain.Model/Entities/Cuentas/Modificación.cs
@@ -3,10 +3,22 @@ using System;
 
 namespace Domain.Model.Entities.Cuentas
 {
+    /// <summary>
+    /// Clase Modificacion
+    /// </summary>
     public class Modificación
     {
+        /// <summary>
+        /// TipoModificación
+        /// </summary>
         public TipoModificación TipoModificación { get; set; }
+        /// <summary>
+        /// UsuarioModificación
+        /// </summary>
         public Usuario UsuarioModificación { get; set; }
+        /// <summary>
+        /// FechaModificación
+        /// </summary>
         public DateTime FechaModificación { get; set; }
     }
 }
--- TallerBackGrupalBOT/src/Domain/Domain.Model/Entities/Cuentas/TipoModificación.cs
+++ TallerBackGrupalBOT/src/Domain/Domain.Model/Entities/Cuentas/TipoModificación.cs
@@ -1,11 +1,29 @@
 ﻿namespace Domain.Model.Entities.Cuentas
 {
+    /// <summary>
+    /// Enum TipoModificación
+    /// </summary>
     public enum TipoModificación
     {
+        /// <summary>
+        /// Habilitación
+        /// </summary>
         Habilitación,
+        /// <summary>
+        /// Inhabilitación
+        /// </summary>
         Inhabilitación,
+        /// <summary>
+        /// Creación
+        /// </summary>
         Creación,
+        /// <summary>
+        /// Activación
+        /// </summary>
         Activación,
+        /// <summary>
+        /// Cancelación
+        /// </summary>
         Cancelación
     }
 }

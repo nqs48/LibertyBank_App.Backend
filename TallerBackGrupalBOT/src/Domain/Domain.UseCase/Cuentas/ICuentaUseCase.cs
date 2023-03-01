668ee1ee1c6526a1742460e67ea6b9d9348316f3
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-02-28 22:47:31 -0500

    Se agrgan metodos a la interface usecase cuenta

--- TallerBackGrupalBOT/src/Domain/Domain.UseCase/Cuentas/ICuentaUseCase.cs
+++ TallerBackGrupalBOT/src/Domain/Domain.UseCase/Cuentas/ICuentaUseCase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Model.Entities.Cuentas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace Domain.UseCase.Cuentas
 {
-    internal interface ICuentaUseCase
+    /// <summary>
+    /// Interface Cuenta UseCase
+    /// </summary>
+    public interface ICuentaUseCase
     {
+        /// <summary>
+        /// Cancelar una Cuenta.
+        /// <param name="idCuenta"></param> 
+        /// </summary>
+        /// <returns></returns>
+        Task<Cuenta> CancelarCuenta(string idCuenta);
+
+        /// <summary>
+        /// Habilitar una Cuenta.
+        /// <param name="idCuenta"></param>
+        /// </summary>
+        /// <returns></returns>
+        Task<Cuenta> HabilitarCuenta(string idCuenta);
+
+        /// <summary>
+        /// Deshabilitar una Cuenta.
+        /// <param name="idCuenta"></param>
+        /// </summary>
+        /// <returns></returns>
+        Task<Cuenta> DeshabilitarCuenta(string idCuenta);
     }
 }

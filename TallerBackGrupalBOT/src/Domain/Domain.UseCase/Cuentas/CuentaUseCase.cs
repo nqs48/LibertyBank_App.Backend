74bb1f588cc4cb1c92c9911680d1cccf022af0a0
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-02-28 22:48:01 -0500

    Implementacion de interface usecase cuenta

--- TallerBackGrupalBOT/src/Domain/Domain.UseCase/Cuentas/CuentaUseCase.cs
+++ TallerBackGrupalBOT/src/Domain/Domain.UseCase/Cuentas/CuentaUseCase.cs
@@ -1,6 +1,54 @@
-﻿namespace Domain.UseCase.Cuentas
+﻿using Domain.Model.Entities.Cuentas;
+using Domain.Model.Entities.Gateway;
+using System.Threading.Tasks;
+
+namespace Domain.UseCase.Cuentas
 {
-    public class CuentaUseCase
+    /// <summary>
+    /// Cuenta UseCase
+    /// </summary>
+    public class CuentaUseCase : ICuentaUseCase
     {
+
+        private readonly ICuentaRepository _repositoryCuenta;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CuentaUseCase"/> class.
+        /// </summary>
+        /// <param name="repositoryCuenta">The logger.</param>
+        public CuentaUseCase( ICuentaRepository repositoryCuenta)
+        {
+            _repositoryCuenta = repositoryCuenta;
+        }
+
+        /// <summary>
+        /// <see cref="ICuentaUseCase.CancelarCuenta(string)"/>
+        /// </summary>
+        /// <param name="idCuenta"></param>
+        /// <returns></returns>
+        public Task<Cuenta> CancelarCuenta(string idCuenta)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// <see cref="ICuentaUseCase.DeshabilitarCuenta(string)"/>
+        /// </summary>
+        /// <param name="idCuenta"></param>
+        /// <returns></returns>
+        public Task<Cuenta> DeshabilitarCuenta(string idCuenta)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// <see cref="ICuentaUseCase.HabilitarCuenta(string)"/>
+        /// </summary>
+        /// <param name="idCuenta"></param>
+        /// <returns></returns>
+        public Task<Cuenta> HabilitarCuenta(string idCuenta)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }

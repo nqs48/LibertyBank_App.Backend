6bffa4358645d26993b0a4f1e9a4bc0ea8e1f295
Juan Pablo Cano <canojuan0415@gmail.com>
2023-03-02 16:02:31 -0500

    Feat: Added Transacción Handler

---
+++ TallerBackGrupalBOT/src/Infrastructure/EntryPoints/EntryPoints.ReactiveWeb/Entities/Handlers/TransaccionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Domain.Model.Entities.Transacciones;
+
+namespace EntryPoints.ReactiveWeb.Entities.Handlers;
+
+/// <summary>
+/// Handler DTO de entidad <see cref="Transacción"/>
+/// </summary>
+public class TransacciónHandler
+{
+    public string Id { get; set; }
+    public string IdCuenta { get; set; }
+    public DateTime FechaMovimiento { get; set; }
+    public TipoTransacción TipoTransacción { get; set; }
+    public decimal Valor { get; set; }
+    public decimal SaldoInicial { get; set; }
+    public decimal SaldoFinal { get; set; }
+    public string Descripción { get; set; }
+}

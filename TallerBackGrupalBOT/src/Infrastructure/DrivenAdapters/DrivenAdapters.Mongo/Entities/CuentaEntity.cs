36a120283d5658d5570f439edbe0b9ac0734f4ca
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-03-03 10:18:57 -0500

    Actualizando propiedades de CuentaEntity

--- TallerBackGrupalBOT/src/Infrastructure/DrivenAdapters/DrivenAdapters.Mongo/Entities/CuentaEntity.cs
+++ TallerBackGrupalBOT/src/Infrastructure/DrivenAdapters/DrivenAdapters.Mongo/Entities/CuentaEntity.cs
@@ -1,10 +1,14 @@
-﻿using DrivenAdapters.Mongo.Entities.Base;
+﻿using Domain.Model.Entities.Clientes;
+using Domain.Model.Entities.Cuentas;
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DrivenAdapters.Mongo.Entities.Base;
 
 namespace DrivenAdapters.Mongo.Entities
 {
@@ -16,9 +20,11 @@ namespace DrivenAdapters.Mongo.Entities
     {
 
         /// <summary>
-        /// Id Cliente
+        /// Id de entidad <see cref="Cliente"/>
         /// </summary>
+        [JsonProperty("idCliente")]
         [BsonElement("idCliente")]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string IdCliente { get; set; }
 
         /// <summary>
@@ -27,16 +33,47 @@ namespace DrivenAdapters.Mongo.Entities
         [BsonElement("numeroCuenta")]
         public string NumeroCuenta { get; set; }
 
+        /// <summary>
+        /// Enum <see cref="TipoCuenta"/>
+        /// </summary>
+        [JsonConverter(typeof(TipoCuenta))]
+        [BsonRepresentation(BsonType.String)]
+        public TipoCuenta TipoCuenta { get; set; }
+
+        /// <summary>
+        /// Enum <see cref="EstadoCuenta"/>
+        /// </summary>
+        [JsonConverter(typeof(EstadoCuenta))]
+        [BsonRepresentation(BsonType.String)]
+        public EstadoCuenta EstadoCuenta { get; set; }
+
+        /// <summary>
+        /// Saldo
+        /// </summary>
+        [BsonElement("saldo")]
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Saldo { get; set; }
+
+        /// <summary>
+        /// Saldo Disponible
+        /// </summary>
+        [BsonElement("saldo_disponible")]
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal SaldoDisponible { get; set; }
+
+        /// <summary>
+        /// Excenta GMF
+        /// </summary>
+        [BsonElement("exenta")]
+        public bool Exenta { get; private set; }
+
+        /// <summary>
+        /// Historial de Modificaciones de la cuneta
+        /// </summary>
+        [BsonElement("historial_modificaciones")]
+        public List<Modificación> HistorialModificaciones { get; private set; }
+
 
     }
 
-
-    /// <param name="id"></param>
-    /// <param name="idCliente"></param>
-    /// <param name="numeroCuenta"></param>
-    /// <param name="tipoCuenta"></param>
-    /// <param name="estadoCuenta"></param>
-    /// <param name="saldo"></param>
-    /// <param name="saldoDisponible"></param>
-    /// <param name="exenta"></param>
 }

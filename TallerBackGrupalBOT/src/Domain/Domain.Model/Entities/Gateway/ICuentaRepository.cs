5dd596ccacb01c465941ddef19534c39f9e97ba8
Juan Pablo Cano <canojuan0415@gmail.com>
2023-03-07 12:55:37 -0500

    Refactor: Added XML comments

--- TallerBackGrupalBOT/src/Domain/Domain.Model/Entities/Gateway/ICuentaRepository.cs
+++ TallerBackGrupalBOT/src/Domain/Domain.Model/Entities/Gateway/ICuentaRepository.cs
@@ -4,14 +4,37 @@ using System.Threading.Tasks;
 
 namespace Domain.Model.Entities.Gateway
 {
+    /// <summary>
+    /// Interfaz de repositorio de entidad <see cref="Cuenta"/>
+    /// </summary>
     public interface ICuentaRepository
     {
+        /// <summary>
+        /// Método para obtener entidades de tipo <see cref="Cuenta"/>
+        /// </summary>
+        /// <returns></returns>
         Task<List<Cuenta>> ObtenerTodos();
 
-        Task<Cuenta> ObtenerPorId(string IdCuenta);
+        /// <summary>
+        /// Método para obtener una entidad de tipo <see cref="Cuenta"/> por su Id
+        /// </summary>
+        /// <param name="idCuenta"></param>
+        /// <returns></returns>
+        Task<Cuenta> ObtenerPorId(string idCuenta);
 
+        /// <summary>
+        /// Método para crear una entidad de tipo <see cref="Cuenta"/>
+        /// </summary>
+        /// <param name="cuenta"></param>
+        /// <returns></returns>
         Task<Cuenta> Crear(Cuenta cuenta);
 
-        Task<Cuenta> Actualizar(string IdCuenta, Cuenta cuenta);
+        /// <summary>
+        /// Método para actualizar una entidad de tipo <see cref="Cuenta"/> por su Id
+        /// </summary>
+        /// <param name="idCuenta"></param>
+        /// <param name="cuenta"></param>
+        /// <returns></returns>
+        Task<Cuenta> Actualizar(string idCuenta, Cuenta cuenta);
     }
 }
--- TallerBackGrupalBOT/src/Infrastructure/EntryPoints/EntryPoints.ReactiveWeb/Entities/Handlers/TransaccionHandler.cs
+++ TallerBackGrupalBOT/src/Infrastructure/EntryPoints/EntryPoints.ReactiveWeb/Entities/Handlers/TransaccionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Domain.Model.Entities.Cuentas;
 using Domain.Model.Entities.Transacciones;
 
 namespace EntryPoints.ReactiveWeb.Entities.Handlers;
@@ -8,12 +9,43 @@ namespace EntryPoints.ReactiveWeb.Entities.Handlers;
 /// </summary>
 public class TransacciónHandler
 {
+    /// <summary>
+    /// Id
+    /// </summary>
     public string Id { get; set; }
+
+    /// <summary>
+    /// Id de entidad <see cref="Cuenta"/>
+    /// </summary>
     public string IdCuenta { get; set; }
+
+    /// <summary>
+    /// Fecha en que se hizo el movimiento
+    /// </summary>
     public DateTime FechaMovimiento { get; set; }
+
+    /// <summary>
+    /// Tipo de transacción
+    /// </summary>
     public TipoTransacción TipoTransacción { get; set; }
+
+    /// <summary>
+    /// Valor
+    /// </summary>
     public decimal Valor { get; set; }
+
+    /// <summary>
+    /// Saldo inicial
+    /// </summary>
     public decimal SaldoInicial { get; set; }
+
+    /// <summary>
+    /// Saldo final
+    /// </summary>
     public decimal SaldoFinal { get; set; }
+
+    /// <summary>
+    /// Descripción
+    /// </summary>
     public string Descripción { get; set; }
 }

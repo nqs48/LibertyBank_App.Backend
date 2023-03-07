1776fe1fa2cb286eff54b1b484f1bfe30a0e2bfd
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-03-06 21:01:09 -0500

    Se agrega documentacion a la interface del repositorio

--- TallerBackGrupalBOT/src/Domain/Domain.Model/Entities/Gateway/ICuentaRepository.cs
+++ TallerBackGrupalBOT/src/Domain/Domain.Model/Entities/Gateway/ICuentaRepository.cs
@@ -4,14 +4,38 @@ using System.Threading.Tasks;
 
 namespace Domain.Model.Entities.Gateway
 {
+    /// <summary>
+    /// Repositorio de la cuenta
+    /// </summary>
     public interface ICuentaRepository
     {
+        /// <summary>
+        /// Obtiene todos las cuentas
+        /// </summary>
+        /// <returns></returns>
         Task<List<Cuenta>> ObtenerTodos();
 
+        /// <summary>
+        /// Obtiene cuenta por Id
+        /// </summary>
+        /// <param name="IdCuenta"></param>
+        /// <returns></returns>
         Task<Cuenta> ObtenerPorId(string IdCuenta);
 
+        /// <summary>
+        /// Crea una nueva cuenta
+        /// </summary>
+        /// <param name="cuenta"></param>
+        /// <returns></returns>
         Task<Cuenta> Crear(Cuenta cuenta);
 
+
+        /// <summary>
+        /// Actualiza datos de la cuenta
+        /// </summary>
+        /// <param name="IdCuenta"></param>
+        /// <param name="cuenta"></param>
+        /// <returns></returns>
         Task<Cuenta> Actualizar(string IdCuenta, Cuenta cuenta);
     }
 }

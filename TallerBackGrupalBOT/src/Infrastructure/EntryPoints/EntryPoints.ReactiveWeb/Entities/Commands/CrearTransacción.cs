60df0ae7bdb4779e2b7690db2b1771d75365cfb1
Juan Pablo Cano <canojuan0415@gmail.com>
2023-03-07 12:49:19 -0500

    Feat: Added Crear transaccion command

---
+++ TallerBackGrupalBOT/src/Infrastructure/EntryPoints/EntryPoints.ReactiveWeb/Entities/Commands/CrearTransacción.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Domain.Model.Entities.Cuentas;
+using Domain.Model.Entities.Transacciones;
+
+namespace EntryPoints.ReactiveWeb.Entities.Commands;
+
+/// <summary>
+/// Comando para crear una entidad de tipo <see cref="Transacción"/>
+/// </summary>
+public class CrearTransacción
+{
+    /// <summary>
+    /// Id de entidad <see cref="Cuenta"/>
+    /// </summary>
+    public string IdCuenta { get; set; }
+    
+    /// <summary>
+    /// Fecha en que se hizo el movimiento
+    /// </summary>
+    public DateTime FechaMovimiento { get; set; }
+    
+    /// <summary>
+    /// Tipo de transacción
+    /// </summary>
+    public TipoTransacción TipoTransacción { get; set; }
+    
+    /// <summary>
+    /// Valor
+    /// </summary>
+    public decimal Valor { get; set; }
+    
+    /// <summary>
+    /// Saldo inicial
+    /// </summary>
+    public decimal SaldoInicial { get; set; }
+    
+    /// <summary>
+    /// Saldo final
+    /// </summary>
+    public decimal SaldoFinal { get; set; }
+    
+    /// <summary>
+    /// Descripción
+    /// </summary>
+    public string Descripción { get; set; }
+}

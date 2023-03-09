f9730bb99771d4f0b6b147c966c1d467cb9881dd
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-03-09 11:02:07 -0500

    Test: testeo de la entidad cuenta CuentaTest

---
+++ TallerBackGrupalBOT/Tests/Domain/Domain.Model.Tests/Entities/CuentaTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Domain.Model.Tests.Entities
+{
+    [ExcludeFromCodeCoverage]
+    public class CuentaTest
+    {
+        [Fact]
+        public void CreaCuenta_coorectamente()
+        {
+        }
+    }
+}
